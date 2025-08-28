599dc3c42a9d268b1a71708e75dc19ab8d2ecfa6
minhnk <minhnk7@gitlab.dsc.com>
2025-08-28 16:23:09 +0700

    MVP reached

--- helper.py
+++ helper.py
@@ -1,18 +1,95 @@
 import os
+import uuid
 
-from langchain_community.document_loaders import PyPDFLoader
+from llama_cloud_services import LlamaParse
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain.text_splitter import MarkdownHeaderTextSplitter
 
 class FileStore:
-    def __init__(self, path):
+    def __init__(self, path, llama_parse_key, openai_api_key):
         self.path = path
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+        self.pdf_path = os.path.join(self.path, "pdf")
+        if not os.path.exists(self.pdf_path):
+            os.makedirs(self.pdf_path)
+
+        self.md_path = os.path.join(self.path, "markdown")
+        if not os.path.exists(self.md_path):
+            os.makedirs(self.md_path)
+
+        self.parser = LlamaParse(
+            api_key=llama_parse_key,
+            parse_mode="parse_page_with_agent",
+            model="openai-gpt-5",
+            high_res_ocr=True,
+            adaptive_long_table=True,
+            outlined_table_extraction=True,
+            annotate_links=True,
+            compact_markdown_table=True,
+            extract_layout=False,
+            language="vi"
+        )
+
+        embeddings = OpenAIEmbeddings(
+            model="text-embedding-3-small",
+            api_key=openai_api_key
+        )
+        connection = "postgresql+psycopg://langchain:langchain@localhost:5432/langchain"
+        collection_name = "my_docs"
+        self.vector_store = PGVector(
+            embeddings=embeddings,
+            collection_name=collection_name,
+            connection=connection,
+            use_jsonb=True,
+        )
+
+        headers_to_split_on = [("#", "Header 1"), ("##", "Header 2"), ("###", "Header 3")]
+        self.text_splitter = MarkdownHeaderTextSplitter(
+            headers_to_split_on=headers_to_split_on,
+            strip_headers=False
+        )
 
     def file_present(self):
-        return len(os.listdir(self.path)) > 0
+        return len(os.listdir(self.md_path)) > 0
 
-    def get_file(self):
-        first_filename = os.listdir(self.path)[0]
-        first_filepath = os.path.join(self.path, first_filename)
+    def add_pdf(self, files):
+        mds = []
 
-        loader = PyPDFLoader(first_filepath)
+        for file in files:
+            filename = file.name
+            safe_name = filename.replace(os.path.sep, "_")
+            save_path = os.path.join(self.pdf_path, safe_name)
 
-        return loader.load()[0]
+            if not os.path.exists(save_path):
+                with open(save_path, "wb") as f:
+                    f.write(file.read())
+
+                md_results = self.parser.parse([os.path.join(self.pdf_path, i) for i in os.listdir(self.pdf_path)])
+                langchain_doc = []
+
+                for md in md_results:
+                    md_docs = md.get_markdown_documents(split_by_page=True)
+
+                    for count, doc in enumerate(md_docs):
+                        path = os.path.join(self.md_path, safe_name.replace(".pdf", f"_{count}.md"))
+                        mds.append(path)
+
+                        with open(path, "w") as f:
+                            f.write(doc.text)
+
+                        langchain_doc.append(
+                            Document(
+                                page_content=doc.text,
+                                metadata={"source": filename},
+                                id=str(uuid.uuid4())
+                            )
+                        )
+
+                self.vector_store.add_documents(langchain_doc)
+
+    def get_json(self, prompt):
+        return self.vector_store.similarity_search(prompt)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,104 @@
 import streamlit as st
-import json
 import os
 
 from openai import OpenAI
 from helper import FileStore
 
+st.set_page_config(layout="wide")
+
 tab_main, tab_file = st.tabs(["Main", "File Upload"])
 
-fileStore = FileStore("FileStore")
+llama_parse_key = st.secrets["llama_parse_key"]
+openai_api_key = st.secrets["openai_api_key"]
+client = OpenAI(api_key=openai_api_key)
+
+UPLOAD_FOLDER = "FileStore"
+fileStore = FileStore(
+    UPLOAD_FOLDER,
+    llama_parse_key,
+    openai_api_key
+)
 
 with tab_main:
     st.title("📄 Document question answering")
 
-    openai_api_key = st.secrets["openai_api_key"]
-    client = OpenAI(api_key=openai_api_key)
+    document = None
 
-    if (prompt := st.chat_input("Câu hỏi về file:")) and fileStore.file_present():
-        uploaded_file = fileStore.get_file()
-        document = {}
+    if fileStore.file_present():
+        col1, col2 = st.columns(
+            [0.6, 0.4],
+            gap="medium",
+            border=True
+        )
 
-        init_prompt = [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": prompt
-                    }
-                ]
-            }, {
-                "role": "developer",
-                "content": f"Extract information from the following json and answer questions: {document}"
-            }
-        ]
+        with col1:
+            if (prompt := st.chat_input("Câu hỏi về file:")):
+                uploaded_file = fileStore.get_json(prompt)
 
-        with st.chat_message("user"):
-            st.markdown(prompt)
+                document = "/n/n".join([file.page_content for file in uploaded_file])
 
-        st.write("Current File:", uploaded_file.metadata["source"].split("/")[-1])
+                with st.chat_message("user"):
+                    st.markdown(prompt)
 
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model="gpt-5-nano",
-                messages=init_prompt,
-                stream=True
-            )
+                st.write("Current File:", ", ".join(list(set([file.metadata["source"] for file in uploaded_file]))))
 
-            response = st.write_stream(stream)
+                with st.chat_message("assistant"):
+                    stream = client.chat.completions.create(
+                        model="gpt-5-nano",
+                        messages=[
+                            {
+                                "role": "developer",
+                                "content": f"Extract information from the following markdown and answer questions: {document}"
+                            }, {
+                                "role": "user",
+                                "content": prompt
+                            }
+                        ],
+                        stream=True
+                    )
 
-UPLOAD_FOLDER = "FileStore"
+                    response = st.write_stream(stream)
 
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+        with col2:
+            st.subheader("References:")
+            if document is None:
+                st.write("...")
+            else:
+                st.write(document)
+    else:
+        st.write("Chưa có file, up bên tab File Upload")
 
 with tab_file:
     st.title("Upload files (pdf)")
 
     uploaded_files = st.file_uploader(
         "Choose a PDF file",
-        type=["pdf"],
+        type="pdf",
         accept_multiple_files=True
     )
 
-    for uploaded_file in uploaded_files:
-        save_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
+    if uploaded_files:
+        with st.spinner("Processing...", show_time=True):
+            fileStore.add_pdf(uploaded_files)
 
-        with open(save_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
+        st.markdown('Upload complete!')
+        st.markdown(
+            '''
+                <style>
+                    .stFileUploaderFile {display: none}
+                <style>
+            ''',
+            unsafe_allow_html=True
+        )
+
+        uploaded_files = False
+
+    st.divider()
+
+    items = os.listdir(fileStore.pdf_path)
+
+    with st.container(border=True):
+        st.subheader("Uploaded", anchor=False)
+
+        for item in items:
+            st.markdown("- " + item)
